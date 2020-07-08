1c4f965bb8722a26f44d6e2c0b1de73723850cd0
tatsuya828828 <kkjy011894@gmail.com>
2020-07-08 13:15:59 +0900

    [finish]9-5

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
-import java.io.FileOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
-		FileOutputStream fos = new FileOutputStream("rpgsave.dat", true);
-		// 65は2進数で01000001
-		fos.write(65);
-		fos.flush();
-		fos.close();
+		String msg = "第1土曜";
+		Reader sr = new StringReader(msg);
+		char c1 = (char) sr.read();
+		char c2 = (char) sr.read();
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		baos.write(65);
+		baos.write(66);
+		byte[] data = baos.toByteArray();
 	}
 }
