7443a13650cc969a9ad9c136a3a1f8ee994c02b4
tatsuya828828 <kkjy011894@gmail.com>
2020-07-08 12:36:05 +0900

    [finish]9-3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
-		FileReader fw = new FileReader("c:¥¥rpgsave.dat");
-		System.out.println("全てのデータを読み込んで表示します。");
-		int i = fw.read();
-		while(i != -1) {
-			char c = (char) i;
-			System.out.print(c);
-			i = fw.read();
-		}
-		System.out.println("");
-		System.out.println("ファイルの末尾に到達しました。");
-		fw.close();
+		FileOutputStream fos = new FileOutputStream("rpgsave.dat", true);
+		// 65は2進数で01000001
+		fos.write(65);
+		fos.flush();
+		fos.close();
 	}
 }
