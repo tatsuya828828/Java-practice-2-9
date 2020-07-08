81b63a8693cfc1bab43f9cbdd864f1d27f1e415f
tatsuya828828 <kkjy011894@gmail.com>
2020-07-08 12:25:12 +0900

    [finish]9-2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
-		FileWriter fw = new FileWriter("c:¥¥rpgsave.dat", true);
-		fw.write("2回目のセーブ");
-		fw.flush();
+		FileReader fw = new FileReader("c:¥¥rpgsave.dat");
+		System.out.println("全てのデータを読み込んで表示します。");
+		int i = fw.read();
+		while(i != -1) {
+			char c = (char) i;
+			System.out.print(c);
+			i = fw.read();
+		}
+		System.out.println("");
+		System.out.println("ファイルの末尾に到達しました。");
 		fw.close();
 	}
 }
